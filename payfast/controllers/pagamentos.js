41224b1d787dfe78d0b9cd3bc321e1c149c6e855
Rafael <rafael22fs@gmail.com>
2019-11-19 18:36:25 -0300

    'Donwload'

--- payfast/controllers/pagamentos.js
+++ payfast/controllers/pagamentos.js
@@ -1,7 +1,53 @@
+
+const http = require('http');
+const fs = require('fs');
+const excel = require('excel4node');
+
 module.exports = function(app){
+
   app.get('/pagamentos', function(req, res){
     console.log('Recebida requisicao de teste na porta 3000.')
-    res.send('OK.');
+    res.send('OK senta la claudinha.');
+  });
+
+  app.get('/report/download', function(req, res){
+    console.log('Preparando para realizar download Report.')
+
+    // Create a new instance of a Workbook class
+    var workbook = new excel.Workbook();
+
+    // Add Worksheets to the workbook
+    var worksheet = workbook.addWorksheet('Sheet222 1');
+    var worksheet2 = workbook.addWorksheet('Sheet222 2');
+
+    // Create a reusable style
+    var style = workbook.createStyle({
+      font: {
+        color: '#FF0800',
+        size: 12
+      },
+      numberFormat: '$#,##0.00; ($#,##0.00); -'
+    });
+
+    // Set value of cell A1 to 100 as a number type styled with paramaters of style
+    worksheet.cell(1,1).number(100).style(style);
+
+    // Set value of cell B1 to 300 as a number type styled with paramaters of style
+    worksheet.cell(1,2).number(200).style(style);
+
+    // Set value of cell C1 to a formula styled with paramaters of style
+    worksheet.cell(1,3).formula('A1 + B1').style(style);
+
+    // Set value of cell A2 to 'string' styled with paramaters of style
+    worksheet.cell(2,1).string('string').style(style);
+
+    // Set value of cell A3 to true as a boolean type styled with paramaters of style but with an adjustment to the font size.
+    worksheet.cell(3,1).bool(true).style(style).style({font: {size: 14}});
+
+    workbook.write('Excel.xlsx');
+
+    res.download('Excel.xlsx');
+
   });
 
   app.delete('/pagamentos/pagamento/:id', function(req, res){
